2b373ae94badc8af69466dc62727c55d01a0349f
Nisarg Mehta <nm555@cam.ac.uk>
2017-01-28 19:16:15 +0000

    Server test

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request, abort
+from google.cloud import vision
+from PIL import Image
+import io
+
+app = Flask(__name__)
+client = vision.Client()
+
+def _convert_to_image(img_bytearray):
+    return Image.open(io.BytesIO(img_bytearray))
+
+@app.route('/image/v1/get_json', methods=['POST'])
+def get_json():
+    if not request.json or not 'image' in request.json:
+        abort(400)
+    image = _convert_to_image(request.json['image'])
+    image.save('Image.jpg')
+    image = client.image(filename='Image.jpg')
+    texts = image.detect_text()
+    return jsonify({"Text":texts})
