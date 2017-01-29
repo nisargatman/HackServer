84e4f156c0212e50c6d62172aa710a997198b806
Nisarg Mehta <nm555@cam.ac.uk>
2017-01-29 12:21:34 +0000

    Error correction

--- server.py
+++ server.py
@@ -92,7 +92,7 @@ def _local_encode(output):
                     pass
                 if isinstance(num,float) and '.' in val:
                     price = num
-                elif "£" in val:
+                elif u"£" in val:
                     price = float(val[1:])
                 total = total + price
             counter = counter + 1
